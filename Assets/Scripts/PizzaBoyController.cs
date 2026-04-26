7ff4f90fe700d82d042125be727479fb9f16d8d6
Cicatrice_Noire <santeliz.ponce@gmail.com>
2026-04-26 17:47:33 +0000

    new file:   Assets/Scripts/PizzaBoyController.cs modified:   README.md

---
+++ Assets/Scripts/PizzaBoyController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PizzaBoyController : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("PizzaBoy is ready!");
+    }
+
+    void Update()
+    {
+        // Add your game logic here
+    }
+}
